195237f36a62b68dc8bd703280ce7c963af67e1c
ArthurNNN <nikitinarthur@gmail.com>
2021-04-21 22:01:41 +0200

    v 2.0 for array of the cities

--- app.js
+++ app.js
@@ -3,32 +3,93 @@
 
 const API_KEY = '63727bfa5b324cce98dfb8d2c9fd3f38'
 const urlBase = 'https://api.weatherbit.io/v2.0/current?'
-const CITY = 'Barcelona'
-const lat = 41.3868;
-const lon = 2.1803;
 
-var URL = urlBase + '&lat=' + lat + '&lon=' + lon + '&key=' + API_KEY;
+// var latitude = 41.3868;
+// var longitude = 2.1803;
 
-const postInfo = (responseJson) => {
+const cities = [];
+cities.push({
+    name: "Barcelona",
+    latitude: 41.390205,
+    longitude: 2.154007,
+});
+cities.push({
+    name: "Madrid",
+    latitude: 40.41,
+    longitude: -3.70,
+});
+cities.push({
+    name: "Valencia",
+    latitude: 39.47,
+    longitude: -0.38
+});
 
-    let forecast = responseJson.data[0];
+
+
+// const getUserPosition = () => {
+//     navigator.geolocation.getCurrentPosition((location) => {
+//         console.log(location);
+//         const { coords: { latitude, longitude } } = location;
+//         getForecast(latitude, longitude);
+//     }, (error) => console.error(error), { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 });
+// }
+
+const render = (responseJson) => {
+
+    let weather = responseJson.data[0];
+    console.log('Success: ');
+    console.log(weather);
+
+    //     <div class="container">
+    //     <div class="app-title">
+    //         <p>Weather</p>
+    //     </div>
+    //     <div class="notification"> </div>
+    //     <div class="weather-container">
+    //         <div class="weather-icon">
+    //             <img src="icons/unknown.png" alt="">
+    //         </div>
+    //         <div class="temperature-value">
+    //             <p>- °<span>C</span></p>
+    //         </div>
+    //         <div class="temperature-description">
+    //             <p> - </p>
+    //         </div>
+    //         <div class="location">
+    //             <p>-</p>
+    //         </div>
+    //     </div>
+    // </div>
+
+    // let div = document.createElement('div.container')
+
+    // let div = document.createElement('div');
+    // div.className = 'container';
+    // let text = document.createTextNode('Test');
+    // div.appendChild(text);
+    // document.body.appendChild(div)
+    // let divContainer = document.querySelector('body:last-child').createElement('div.container')
 
     let pCity = document.querySelector('div.app-title p');
-    pCity.textContent = CITY;
+    pCity.textContent = weather.city_name;
 
     let img = document.querySelector("div.weather-icon img");
-    let iconUrl = 'https://www.weatherbit.io/static/img/icons/' + forecast.weather.icon + '.png';
+    let iconUrl = 'https://www.weatherbit.io/static/img/icons/' + weather.weather.icon + '.png';
     img.src = iconUrl;
 
     let ptemperature = document.querySelector('div.temperature-value p');
-    ptemperature.textContent = forecast.temp;
+    ptemperature.textContent = weather.temp;
 
     let pDescription = document.querySelector('div.temperature-description p');
-    pDescription.textContent = forecast.weather.description;
+    pDescription.textContent = weather.weather.description;
 
     let pLocation = document.querySelector('div.location p');
-    pLocation.textContent = '[' + forecast.city_name + ']';
+    pLocation.textContent = '[' + weather.city_name + ', ' + weather.country_code + ']';
 
+    let div = document.createElement('div');
+    // div.className = 'container';
+
+    duplicateChildNodes('body');
 }
 
 function handleErrors(response) {
@@ -38,12 +99,27 @@ function handleErrors(response) {
     return response;
 }
 
-const getForecast = () => {
+const getWeather = (latitude, longitude) => {
+    const URL = `${urlBase}&lat=${latitude}&lon=${longitude}&key=${API_KEY}`;
     fetch(URL)
         .then(handleErrors)
         .then(response => response.json())
-        .then(postInfo)
+        .then(render)
         .catch((error) => { console.log(error) })
 }
 
-getForecast();
+function duplicateChildNodes(parentId) {
+    var parent = document.querySelector(parentId);
+    NodeList.prototype.forEach = Array.prototype.forEach;
+    var children = parent.childNodes;
+    children.forEach(function (item) {
+        var cln = item.cloneNode(true);
+        parent.appendChild(cln);
+    });
+};
+
+// duplicateChildNodes("container");
+
+// getUserPosition();
+
+cities.forEach((city) => getWeather(city.latitude, city.longitude));
